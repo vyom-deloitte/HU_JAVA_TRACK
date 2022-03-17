3117740a2b906f834df7b4d274d0153f115d6464
vyom-deloitte <vypant@deloitte.com>
2022-03-17 11:18:23 +0530

    Update and rename Strings.java to Strings_Assignment.java

--- Strings_Assignment.java
+++ Strings_Assignment.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
  
-public class Strings {
+public class Strings_Assignment {
     public static void main(String []args){
         Scanner sc = new Scanner(System.in);
         String s1 = sc.nextLine();
